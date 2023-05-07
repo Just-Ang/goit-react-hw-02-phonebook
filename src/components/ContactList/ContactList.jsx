4a83594d9a10c6bdd93e4ed0c00b593283831530
Angelica <lisoveckaya1997@gmail.com>
2023-05-07 13:31:51 +0300

    fix

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,4 +1,5 @@
 import css from './ContactList.module.css';
+import PropTypes from 'prop-types';
 
 export const ContactList = ({ contacts, onDeleteContact }) => {
   return (
@@ -18,3 +19,12 @@ export const ContactList = ({ contacts, onDeleteContact }) => {
     </ul>
   );
 };
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    }))
+}
